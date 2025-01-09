5467c0616039d399670d05a88c683d5bbc68912a
Chanwoo Kim <kcw2205@naver.com>
2025-01-09 23:32:09 +0900

    [Bronze I] Title: A/B - 2, Time: 0 ms, Memory: 2020 KB, Score: 18 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cout.tie(0);
+    double a, b; cin>>a>>b;
+
+    cout.precision(1000);
+    cout<<a/b;
+}
