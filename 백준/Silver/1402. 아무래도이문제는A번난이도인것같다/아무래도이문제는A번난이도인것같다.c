a1a66e6548d1a86ec864eb9f198ed398cf95963f
Chanwoo Kim <kcw2205@naver.com>
2025-05-08 10:57:52 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.c
@@ -0,0 +1,14 @@
+// 살짝만 생각해보면 간단한 문제...
+// 해를 생각해내는 방법이 문제를 푸는 과정인듯..
+
+#include <stdio.h>
+
+int main() {
+    int T; scanf("%d", &T);
+    while(T--) {
+        int A, B; scanf("%d %d", &A, &B);
+        puts("yes");
+    }
+    
+}
+
