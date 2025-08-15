45e51ddc879d7f816aae4125999953e1b0567271
Chanwoo Kim <kcw2205@naver.com>
2025-08-15 21:00:32 +0900

    [Bronze II] Title: Robot Rock Band (Small), Time: 320 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/12044. Robot Rock Band （Small）/Robot Rock Band （Small）.cc
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define fastio std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr);std::cout.tie(nullptr);
+#define endl '\n'
+using namespace std;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pss = pair<string, string>;
+using pis = pair<int, string>;
+using pci = pair<char, int>;
+using pic = pair<int, char>;
+using qi = queue<int>;
+using qpii = queue<pii>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using ll = long long;
+
+int check_k(vvi robot_maps, int place, int n, int k, int val) {
+    int result = 0;
+
+    for(int i=0; i<n; ++i) {
+        if (place < 3) result += check_k(robot_maps, place + 1, n, k, place == 0 ? robot_maps[place][i] : (val ^ robot_maps[place][i]));
+        else result += ((val ^ robot_maps[place][i]) == k) ? 1 : 0;
+    }
+
+    return result;
+}
+
+int main(){
+    fastio
+
+    int tc; cin>>tc;
+
+    for(int t = 1; t<=tc; ++t) {
+        int n, k; cin>>n>>k;
+        vvi robot_maps(4, vi(n));
+
+        for(int i=0; i<4; ++i) {
+            for(int j=0; j<n; ++j) {
+                cin>>robot_maps[i][j];
+            }
+        }
+
+        cout<<"Case #"<<t<<": "<<check_k(robot_maps, 0, n, k, 0)<<endl;
+    }
+    
+}
