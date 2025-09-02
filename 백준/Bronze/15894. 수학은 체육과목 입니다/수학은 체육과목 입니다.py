e4ee995cb48a05401e2e7f8bcc305b37e99b388c
Chanwoo Kim <kcw2205@naver.com>
2025-09-02 23:17:16 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.py
@@ -0,0 +1,8 @@
+from sys import stdin, setrecursionlimit
+rr = lambda: stdin.readline().rstrip("\r\n")
+# uncomment if u use recursion
+# sys.setrecursionlimit(10 ** 6)
+
+n = int(rr())
+
+print(f"{4*n}")
