e891bcf7f41997150c03d2d13433faaa187803e0
Chanwoo Kim <kcw2205@naver.com>
2025-09-17 00:26:10 +0900

    [Gold IV] Title: 카카오톡, Time: 852 ms, Memory: 53920 KB -BaekjoonHub

--- 백준/Gold/20943. 카카오톡/카카오톡.py
+++ 백준/Gold/20943. 카카오톡/카카오톡.py
@@ -1,34 +1,12 @@
 from sys import stdin, setrecursionlimit
 rr = lambda: stdin.readline().rstrip("\r\n")
 
-def euclid(a, b):
-    if a > b:
-        a, b = b, a
-
-    if a == 0:
-        return b
-
-    return euclid(b%a, a)
-
 n = int(rr())
 diffs = {} # lines[a][b] = count
 horizontal = 0
 
 for i in range(n):
     a, b, c = map(int, rr().split())
-    
-    # a, b 를 서로소로 만들기
-    mod = euclid(abs(a), abs(b))
-
-    ua = abs(a)//mod
-    ub = abs(b)//mod
-    
-    if (a < 0 and b > 0) or (a > 0 and b < 0):
-        a = ua
-        b = -ub
-    else:
-        a = ua
-        b = ub
     
     if a == 0:
         diffs[float('inf')] = diffs.get(float('inf'), 0) + 1
